516658546c98d9c5db23810a01119526ab3cffcf
ynj <yangn497@gmail.com>
2026-05-21 05:41:53 +0800

    feat: stabilize V431 router platform

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,6 @@
 ﻿import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './V432UnifiedPlatform'
+import App from './V431StableRouter29PlusStable'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
